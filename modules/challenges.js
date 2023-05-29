fdb35072c2df6a4fd7917093ba1564d6cd4155e0
Space_Interprise <44732812+emanuelfranklyn@users.noreply.github.com>
2023-05-28 21:58:48 -0300

    implement challenges

--- modules/challenges.js
+++ modules/challenges.js
@@ -3,17 +3,38 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
-const challenges = [];
+var challenges = [];
+var nextChallengeIn = 0;
 
-fs.readdirSync(path.resolve(__dirname, '..', 'public', 'challenges')).forEach(file => {
-  const challenge = require(path.resolve(__dirname, '..', 'public', 'challenges', file, 'index.json'));
-  challenge.id = file;
-  challenges.push(challenge);
-});
+function loadChallenges() {
+  challenges = [];
+  nextChallengeIn = 0;
+  fs.readdirSync(path.resolve(__dirname, '..', 'public', 'challenges')).forEach(file => {
+    const challenge = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'public', 'challenges', file, 'index.json')));
+    challenge.id = file;
+    var temp = challenge.timestamp.split('/');
+    challenge.timestamp = new Date(temp[1] + '/' + temp[0] + '/' + temp[2] + ' 12:00').getTime();
+    if (challenge.timestamp > Date.now()) {
+      // don't add challenges that are not yet available and set the next challenge timer to the smallest one
+      nextChallengeIn = nextChallengeIn === 0 ? challenge.timestamp : Math.min(nextChallengeIn, challenge.timestamp);
+    } else {
+      challenges.push(challenge);
+    }
+  });
+}
+
+loadChallenges();
+setInterval(() => {
+  loadChallenges();
+// }, 1000 * 60 * 30); // reload challenges every 30 minutes
+}, 1000 * 30); // DEBUG: reload challenges every 30 seconds
 
 // is mapped to /api/challenges/
 app.get('/', (req, res) => {
-  res.send(challenges);
+  res.send({
+    challenges,
+    nextChallengeIn
+  });
 });
 
 app.get('/:id/thumb', (req, res) => {
